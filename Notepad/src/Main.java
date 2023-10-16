5f9d94d7c5c1b35a13bd4ae0b576db299a0451a5
Sinan <sinan_karabulut97@hotmail.com>
2023-10-16 22:26:43 +0300

    NotePad refactor V2

--- Notepad/src/Main.java
+++ Notepad/src/Main.java
@@ -1,40 +1,83 @@
+
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Define the file name where the notes will be stored
+        String fileName = "src/notes.txt";
+
         // Create a Scanner object for user input
         Scanner scanner = new Scanner(System.in);
 
-        // Ask the user to write a note and store it in the 'note' variable
-        System.out.print("Please write your note:");
-        String note = scanner.nextLine();
-
-        // Define the file name where the notes will be stored
-        String fileName = "notes.txt";
+        // Create an ArrayList to store previously written notes
+        ArrayList<String> previousNotes = new ArrayList<>();
 
         try {
-            // Open the file in append mode to add new notes without overwriting existing content
-            FileWriter fileWriter = new FileWriter(fileName, true);
-            PrintWriter printWriter = new PrintWriter(fileWriter);
+            // Open the file in read mode or create the file if it doesn't exist
+            File file = new File(fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+                System.out.println("File created: " + fileName);
+            }
+            System.out.println("Type 'exit' to quit the program.");
+            System.out.println("Type 'clear' to clear the file.\n");
 
-            // Write the user's note to the file and close the PrintWriter
-            printWriter.println(note);
-            printWriter.close();
+            while (true) {
+                // Open the file in read mode
+                FileReader fileReader = new FileReader(fileName);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-            // Open the file in read mode to display the stored notes
-            FileReader fileReader = new FileReader(fileName);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+                // Read and store previously written notes
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    previousNotes.add(line);
+                }
 
-            // Read the first line of the file (assuming there is only one line in the file)
-            String readNotes = bufferedReader.readLine();
+                // Close the BufferedReader after reading
+                bufferedReader.close();
 
-            // Close the BufferedReader after reading
-            bufferedReader.close();
+                // Print previously written notes
+                System.out.println("Previously written notes:");
+                for (String note : previousNotes) {
+                    System.out.println(note);
+                }
+
+                // Ask the user to write a new note
+                System.out.println("Please write your new note:");
+                String note = scanner.nextLine();
+
+                // Check if the user wants to exit the program
+                if (note.equalsIgnoreCase("exit")) {
+                    System.out.println("Exiting notepad. Goodbye!");
+                    break; // Exit the loop if the user enters 'exit'
+                }
+                if (note.equalsIgnoreCase("clear")) {
+                    FileWriter fileWriter = new FileWriter(fileName, false);
+                    PrintWriter printWriter = new PrintWriter(fileWriter);
+                    //printWriter.println("");
+                    printWriter.close();
+                    continue;
+                }
+                // Open the file in append mode to add new notes without overwriting existing content
+                FileWriter fileWriter = new FileWriter(fileName, true);
+                PrintWriter printWriter = new PrintWriter(fileWriter);
+
+                // Write the user's note to the file and close the PrintWriter
+                printWriter.println(note);
+                printWriter.close();
+
+                // Inform the user that the note has been saved
+                System.out.println("Note saved successfully!\n");
+
+                // Clear the ArrayList for the next iteration
+                previousNotes.clear();
+            }
         } catch (IOException e) {
             // Handle any IOException that might occur during file operations
-            // and throw a RuntimeException with the original exception for debugging purposes
-            throw new RuntimeException(e);
+            // Print an error message and terminate the program
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
